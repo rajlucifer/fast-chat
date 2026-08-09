686369702d90088f7d481aebeac38ee5319a664f
rahul raj <thisisrajtkd18@gmail.com>
2026-08-09 19:01:46 +0530

    create the routes for the sendMessag

--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoutes } from "../middleware/auth.js";
-import { getMessages, getUsersForSiderBar, markMessageSeen } from "../controllers/messageController.js";
+import { getMessages, getUsersForSiderBar, markMessageSeen, sendMessage } from "../controllers/messageController.js";
 
 const messageRouter = express.Router();
 
@@ -8,5 +8,6 @@ const messageRouter = express.Router();
 messageRouter.get("/users",protectRoutes,getUsersForSiderBar);
 messageRouter.get("/:id",protectRoutes,getMessages);
 messageRouter.put("/mark/:id",protectRoutes,markMessageSeen);
+messageRouter.post("/send/:id",protectRoutes,sendMessage)
 
 export default messageRouter;
