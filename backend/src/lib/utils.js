0ed318d2d6bc681fb7567cc1a1a2e9e7ae618fb9
rahul raj <thisisrajtkd18@gmail.com>
2026-08-12 19:37:56 +0530

    correct the token generation

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 // function to generate the token for the user
 
-export const generateToken = async(userId)=>{
+export const generateToken = (userId)=>{
     const token = jwt.sign({userId},process.env.JWT_SECRET);
     return token;
 
