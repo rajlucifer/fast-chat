dbc9a0c534286df06f080c603594ad2b17c1109c
rahul raj <thisisrajtkd18@gmail.com>
2026-08-15 14:39:48 +0530

    make the delete routes

--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoutes } from "../middleware/auth.js";
-import { getMessages, getUsersForSiderBar, markMessageSeen, sendMessage } from "../controllers/messageController.js";
+import { getMessages, getUsersForSiderBar, markMessageSeen, sendMessage,deleteChat } from "../controllers/messageController.js";
 
 const messageRouter = express.Router();
 
@@ -8,6 +8,7 @@ const messageRouter = express.Router();
 messageRouter.get("/users",protectRoutes,getUsersForSiderBar);
 messageRouter.get("/:id",protectRoutes,getMessages);
 messageRouter.put("/mark/:id",protectRoutes,markMessageSeen);
-messageRouter.post("/send/:id",protectRoutes,sendMessage)
+messageRouter.post("/send/:id",protectRoutes,sendMessage);
+messageRouter.delete("/delete/:id",protectRoutes,deleteChat)
 
 export default messageRouter;
